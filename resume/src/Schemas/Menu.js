5ddf15ab9814d592f88876133537f737829812fe
Sarun Khumthai <srnkhumthai@gmail.com>
2024-08-17 23:59:49 +0700

    refactor: Update Menu.js to remove commented out code and rearrange menu items

--- resume/src/Schemas/Menu.js
+++ resume/src/Schemas/Menu.js
@@ -6,20 +6,20 @@ export const Menu = {
       className: "bx-home",
     },
     {
-      label: "Profile",
-      section: "#profile",
-      className: "bx-user",
-    },
-    {
       label: "Skills",
       section: "#skills",
       className: "bx-receipt",
     },
     {
-      label: "Experience",
-      section: "#experience",
-      className: "bx-briefcase-alt",
+      label: "Profile",
+      section: "#profile",
+      className: "bx-user",
     },
+    // {
+    //   label: "Experience",
+    //   section: "#experience",
+    //   className: "bx-briefcase-alt",
+    // },
     {
       label: "Education",
       section: "#education",
